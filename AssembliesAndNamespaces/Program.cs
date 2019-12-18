8c3863a7de24aa8034d574ab79db0654be7f832d
JingleDjango95 <spencerweber019@gmail.com>
2019-12-18 10:09:07 -0700

    Added hex, password, and xml checks

--- AssembliesAndNamespaces/Program.cs
+++ AssembliesAndNamespaces/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using static System.Console;
 using System.Xml.Linq;
+using Packt.Shared;
 
 namespace AssembliesAndNamespaces
 {
@@ -9,8 +11,20 @@ namespace AssembliesAndNamespaces
     {
       var doc = new XDocument();
 
-      string s1 = "Hello";
-      String s2 = "World";
+      Write("Enter a color value in hex: ");
+      string hex = ReadLine();
+
+      WriteLine("Is {0} a valid color value? {1}", arg0: hex, arg1: hex.IsValidXmlTag());
+
+      Write("Enter a XML tag: ");
+      string xmlTag = ReadLine();
+
+      WriteLine("Is {0} a valid XML tag? {1}", arg0: xmlTag, arg1: xmlTag.IsValidXmlTag());
+
+      Write("Enter a password: ");
+      string password = ReadLine();
+
+      WriteLine("Is {0} a valid password? {1}", arg0: password, arg1: password.IsValidPassword());
     }
   }
 }
