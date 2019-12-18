9c57527a41092888e93f2df861541e1a5da0e6bd
JingleDjango95 <spencerweber019@gmail.com>
2019-12-18 10:24:51 -0700

    testing a nuget package for compatability

--- AssembliesAndNamespaces/Program.cs
+++ AssembliesAndNamespaces/Program.cs
@@ -2,6 +2,8 @@
 using static System.Console;
 using System.Xml.Linq;
 using Packt.Shared;
+using DialectSoftware.Collections;
+using DialectSoftware.Collections.Generics;
 
 namespace AssembliesAndNamespaces
 {
@@ -25,6 +27,33 @@ namespace AssembliesAndNamespaces
       string password = ReadLine();
 
       WriteLine("Is {0} a valid password? {1}", arg0: password, arg1: password.IsValidPassword());
+
+      var x = new Axis("x", 0, 10, 1);
+      var y = new Axis("y", 0, 4, 1);
+
+      var matrix = new Matrix<long>(new[] { x, y });
+      for (int i = 0; i < matrix.Axes[0].Points.Length; i++)
+      {
+        matrix.Axes[0].Points[i].Label = "x" + i.ToString();
+      }
+
+      for (int i = 0; i < matrix.Axes[1].Points.Length; i++)
+      {
+        matrix.Axes[1].Points[1].Label = "y" + i.ToString();
+      }
+
+      foreach (long[] c in matrix)
+      {
+        matrix[c] = c[0] + c[1];
+      }
+
+      foreach (long[] c in matrix)
+      {
+        WriteLine("{0}, {1} ({2}, {3}) = {4}",
+          matrix.Axes[0].Points[c[0]].Label,
+          matrix.Axes[1].Points[c[1]].Label,
+          c[0], c[1], matrix[c]);
+      }
     }
   }
 }
